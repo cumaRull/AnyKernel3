9c4fe4dfe534479dd2abce5ca565a418382155c2
CumaRull <102359107+cumaRull@users.noreply.github.com>
2024-03-27 06:40:08 +0700

    Removed name spelling errors

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=TridentNothDeath Kernel for realme C25 Series (even)
+kernel.string=TridentNotDeath Kernel for realme C25 Series (even)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
